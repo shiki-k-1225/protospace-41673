f21086a38a15efef31cfad445ba418a8bcfda090
shiki-k-1225 <4949gtmw@gmail.com>
2025-01-14 19:48:49 +0900

    修正２

---
+++ bin/render-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install dependencies
+bundle install
+
+# Precompile assets
+bundle exec rake assets:precompile
+
+# Migrate the database
+bundle exec rake db:migrate
+
+# Build JavaScript assets
+yarn build
+
+# Start Puma server
+bundle exec puma -C config/puma.rb
